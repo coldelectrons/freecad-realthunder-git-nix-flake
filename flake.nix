b945f1a0ba5b460548ac1a70acdedacf62234c55
coldelectrons <frithomas@gmail.com>
2024-04-15 14:47:04 -0400

    changed up flake.nix

--- flake.nix
+++ flake.nix
@@ -2,36 +2,43 @@
   description = "FreeCAD-realthunder package from git";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    freecad-realthunder.url = "github:realthunder/FreeCAD/LinkStable";
-    freecad-realthunder.flake = false;
+    nixpkgs.url = "nixpkgs";
+    freecad-realthunder = {
+      url = "github:realthunder/FreeCAD/LinkStable";
+      flake = false;
+    };
   };
 
-  outputs = { nixpkgs, ... }@inputs: {
-    packages = builtins.listToAttrs (map
-      (system: {
-        name = system;
-        value = with import nixpkgs { inherit system; }; rec {
-
-          default = freecad-realthunder;
-
-          freecad-realthunder = pkgs.libsForQt5.callPackage ./package.nix {
-            boost = python3Packages.boost;
-            inherit (python3Packages)
-              gitpython
-              matplotlib
-              pivy
-              ply
-              pycollada
-              pyside2
-              pyside2-tools
-              python
-              pyyaml
-              scipy
-              shiboken2;
-          };
-
+  outputs = {nixpkgs, ...}:
+  let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (sys: f nixpkgs.legacyPackages.${sys});
+  in rec {
+    packages = forAllSystems (pkgs: {
+      default = pkgs.libsForQt5.callPackage ./package.nix {
+        boost = python3Packages.boost;
+        inherit (python3Packages)
+          gitpython
+          matplotlib
+          pivy
+          ply
+          pycollada
+          pyside2
+          pyside2-tools
+          python
+          pyyaml
+          scipy
+          shiboken2;
+      };
+    });
+    devShells = forAllSystems (pkgs: {
+      default = let
+        inherit (packages.${pkgs.system}) default;
+        inherit (pkgs) lib mkShell;
+      in
+        mkShell {
+          inputsFrom = [default];
         };
-      }) [ "x86_64-linux" "aarch64-linux" ]);
+    });
   };
 }
